ee46bb9ad21feb1446ca51f8d62c0196643fc83a
Ted <zqft9001@gmail.com>
2026-06-28 15:40:44 -0400

    Began work on image caching

---
+++ template_db_defs.php
@@ -0,0 +1,10 @@
+<?php
+//Defines for databases. K.I.S.S.
+//
+
+define('SERVERNAME', 'server hostname');
+define('USERNAME', 'server username');
+define('PASSWORD', 'username password');
+define('DBNAME', 'database name');
+
+?>
